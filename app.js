f7601be071278f52fd577c762ad294bb361bf5ce
yogapermana76 <permana76.yp@gmail.com>
2019-04-02 08:43:41 +0700

    upload heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extends: true}))
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,24 +1,35 @@
 const jwt = require('jsonwebtoken')
-const { User } = require('../models')
+// const { User } = require('../models')
 
 module.exports = {
   isLogedIn(req, res, next) {
+    // try {
+    //   const decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET);
+    //   req.authenticatedUser = decoded
+    //   // User.findByPk(req.params.id)
+    //   //   .then(user => {
+    //   //     if(user && user.username === req.authenticatedUser.username) {
+    //   //       next()
+    //   //     } else {
+    //   //       res.status(401).json({
+    //   //         message: 'tidak bisa akses data ini'
+    //   //       })
+    //   //     }
+    //   //   })
+    //   next()
+    // } catch(err) {
+    //   res.status(401).json(err)
+    // }
+
     try {
-      const decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET)
       req.authenticatedUser = decoded
-      // User.findByPk(req.params.id)
-      //   .then(user => {
-      //     if(user && user.username === req.authenticatedUser.username) {
-      //       next()
-      //     } else {
-      //       res.status(401).json({
-      //         message: 'tidak bisa akses data ini'
-      //       })
-      //     }
-      //   })
       next()
-    } catch(err) {
-      res.status(401).json(err)
+    } 
+    catch {
+      res.status(401).json({
+        message:'Failed to authenticate user'
+      })
     }
   }
 }
